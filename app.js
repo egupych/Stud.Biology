2d16fe2d71157df3b9dc6b0029e216c6b11be1be
Dmitry Egupov <96996225+Egupych@users.noreply.github.com>
2022-04-17 14:14:32 +0500

    Update app.js

--- app.js
+++ app.js
@@ -13,3 +13,35 @@ function clearActiveClasses() {
     slide.classList.remove("active");
   });
 }
+
+
+const cursor1 = document.querySelector(".cursor1");
+document.addEventListener("mousemove", (e) => {
+  cursor1.style.left= e.pageX+"px";
+  cursor1.style.top= e.pageY+"px";
+})
+
+const cursor2 = document.querySelector(".cursor2");
+document.addEventListener("mousemove", (e) => {
+  cursor2.style.left= e.pageX+"px";
+  cursor2.style.top= e.pageY+"px";
+})
+
+const cursor3 = document.querySelector(".cursor3");
+document.addEventListener("mousemove", (e) => {
+  cursor3.style.left= e.pageX+"px";
+  cursor3.style.top= e.pageY+"px";
+})
+
+const cursor4 = document.querySelector(".cursor4");
+document.addEventListener("mousemove", (e) => {
+  cursor4.style.left= e.pageX+"px";
+  cursor4.style.top= e.pageY+"px";
+})
+
+if (window.matchMedia("(max-width: 576px)").matches) {
+  document.getElementById("logo").innerHTML = "Svetlana Podergina";
+} 
+
+
+  
