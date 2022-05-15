6969974b57754412737f33949fe95d20a4b1c3af
Dmitry Egupov <96996225+Egupych@users.noreply.github.com>
2022-05-15 21:44:39 +0500

    Update app.js

--- app.js
+++ app.js
@@ -119,6 +119,38 @@ function setScrollIntoView4Options(top) {
 //   cursor4.style.top= e.pageY+"px";
 // })
 
+const animItems = document.querySelectorAll("._anim-items");
 
+if (animItems.length>0) {
+	window.addEventListener("scroll", animOnScroll);
+	function animOnScroll() {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems [index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 1;
+		
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if ((scrollY> animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('_active');
+			}  else {
+				animItem.classList.remove('_active');
+			}
+
+		}
+	}
+
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+			scrollTop = window.scrollY || document.documentElement.scrollTop;
+		return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+	}
+	animOnScroll();
+}
 
   
